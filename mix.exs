7f77872f3017fc06f741d2958e05d8e303ef8b7c
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-01-16 10:24:46 +0100

    Add doc and package information

--- mix.exs
+++ mix.exs
@@ -8,7 +8,11 @@ defmodule ExDoctor.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "ExDoctor",
+      source_url: "https://github.com/chrzaszcz/ex_doctor",
+      docs: docs()
     ]
   end
 
@@ -22,9 +26,24 @@ defmodule ExDoctor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:erlang_doctor, "~> 0.2"}
+      {:erlang_doctor, "~> 0.2"},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/chrzaszcz/ex_doctor"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
     ]
   end
 end
