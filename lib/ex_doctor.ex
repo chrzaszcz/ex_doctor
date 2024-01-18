faabe9c92135cde49699e54fe93ee1c0a129bc5b
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-01-18 16:27:59 +0100

    Add short module doc for ExDoctor

--- lib/ex_doctor.ex
+++ lib/ex_doctor.ex
@@ -1,6 +1,10 @@
 defmodule ExDoctor do
   require Record
 
+  @moduledoc """
+  This module defines records from the `:tr` module, allowing to use them in `iex`.
+  """
+
   for {name, fields} <- Record.extract_all(from_lib: "erlang_doctor/include/tr.hrl") do
     Record.defrecord(name, fields)
   end
