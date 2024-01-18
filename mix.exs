3e7cc52a48eb4bb59d7f8f843636bffed090ee7a
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-01-18 16:20:44 +0100

    Keep doctor versions in sync

--- mix.exs
+++ mix.exs
@@ -1,11 +1,13 @@
 defmodule ExDoctor.MixProject do
   use Mix.Project
 
+  @version "0.2.3"
+
   def project do
     [
       app: :ex_doctor,
       description: "Lightweight tracing, debugging and profiling utility",
-      version: "0.1.1",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -26,7 +28,7 @@ defmodule ExDoctor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:erlang_doctor, "~> 0.2.2"},
+      {:erlang_doctor, @version},
       {:ex_doc, "~> 0.31", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
