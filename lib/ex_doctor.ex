40aa5f94c86ba0ddf771237a7889dc2be9a7afea
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-01-26 17:54:34 +0100

    Put literal record definitions in ExDoctor to make it self-contained

--- lib/ex_doctor.ex
+++ lib/ex_doctor.ex
@@ -3,9 +3,32 @@ defmodule ExDoctor do
 
   @moduledoc """
   This module defines records from the `:tr` module, allowing to use them in `iex`.
+
+  The records are obtained by calling `Record.extract_all(from_lib: "erlang_doctor/src/tr.erl")`
+
+  This module can be dynamically loaded without access to `tr.erl`, so they are listed literally.
   """
 
-  for {name, fields} <- Record.extract_all(from_lib: "erlang_doctor/include/tr.hrl") do
+  @records [
+    tr: [
+      index: :undefined,
+      pid: :undefined,
+      event: :undefined,
+      mfa: :no_mfa,
+      data: :undefined,
+      ts: :undefined,
+      info: :no_info
+    ],
+    node: [
+      module: :undefined,
+      function: :undefined,
+      args: :undefined,
+      children: [],
+      result: :undefined
+    ]
+  ]
+
+  for {name, fields} <- @records do
     Record.defrecord(name, fields)
   end
 end
